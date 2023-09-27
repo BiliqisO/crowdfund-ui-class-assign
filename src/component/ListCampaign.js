b23cf77af6a1b0922028a0258f502facd61cbbd6
BiliqisO <biliqisonikoyi@gmail.com>
2023-09-27 06:51:48 +0100

    fix

--- src/component/ListCampaign.js
+++ src/component/ListCampaign.js
@@ -1,68 +1,16 @@
 import React, { useEffect, useState } from "react";
 import useContract from "../hooks/useContract";
+import { data } from "../utils/abi";
 
 function ListCampaign() {
-  const contract = useContract("0x46f44F2D1af04D54ab5BCbEF9F4D0Df9baDc1B8C", [
-    {
-      inputs: [{ internalType: "uint256", name: "_ID", type: "uint256" }],
-      name: "campaignEnds",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        { internalType: "uint256", name: "", type: "uint256" },
-        { internalType: "address", name: "", type: "address" },
-      ],
-      name: "contribute",
-      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [{ internalType: "uint256", name: "_ID", type: "uint256" }],
-      name: "contributeEth",
-      outputs: [],
-      stateMutability: "payable",
-      type: "function",
-    },
-    {
-      inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-      name: "crowd",
-      outputs: [
-        { internalType: "string", name: "title", type: "string" },
-        { internalType: "uint256", name: "fundingGoal", type: "uint256" },
-        { internalType: "address", name: "owner", type: "address" },
-        { internalType: "uint256", name: "durationTime", type: "uint256" },
-        { internalType: "bool", name: "isActive", type: "bool" },
-        { internalType: "uint256", name: "fundingBalance", type: "uint256" },
-      ],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [],
-      name: "id",
-      outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        { internalType: "string", name: "_title", type: "string" },
-        { internalType: "uint256", name: "_fundingGoal", type: "uint256" },
-        { internalType: "uint256", name: "_durationTime", type: "uint256" },
-      ],
-      name: "proposeCampaign",
-      outputs: [],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-  ]);
+  const contract = useContract(
+    "0x46f44F2D1af04D54ab5BCbEF9F4D0Df9baDc1B8C",
+    data
+  );
 
   const [campaigns, setCampaigns] = useState([]);
   const [maxCampaignID, setMaxCampaignID] = useState(0);
+  console.log(campaigns);
 
   useEffect(() => {
     console.log("calling fetch", contract);
@@ -105,11 +53,11 @@ function ListCampaign() {
             <strong>Campaign #{index}</strong>
             <ul>
               <li>Title: {campaign.title}</li>
-              <li>Funding Goal: {campaign.fundingGoal} ETH</li>
+              <li>Funding Goal: {campaign.fundingGoal.toString()} ETH</li>
               <li>Owner: {campaign.owner}</li>
-              <li>Duration Time: {campaign.durationTime} seconds</li>
+              <li>Duration Time: {campaign.durationTime.toString()} seconds</li>
               <li>Is Active: {campaign.isActive ? "Yes" : "No"}</li>
-              <li>Funding Balance: {campaign.fundingBalance} ETH</li>
+              <li>Funding Balance: {campaign.fundingBalance.toString()} ETH</li>
             </ul>
           </li>
         ))}
